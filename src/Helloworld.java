a770eba416b4d2c9370985657a50ac7105ef3719
brugernavn <mikk39k1rstud.kea.dk>
2022-10-13 10:49:36 +0200

    First Commit

---
+++ src/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+
+    public static void main(String[] args) {
+        //Brug to skråstreger til at kommentere
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");
+    }
+}
---
+++ src/Variables2.java
@@ -0,0 +1,15 @@
+public class Variables2 {
+
+    public static void main(String[] args){
+
+        int date = 25;
+        String day = "Thursday";
+        String th = "th";
+        String month = "August";
+        int year = 2022;
+
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println(day + " "+ date + " " + month + " " + year );
+
+    }
+}
